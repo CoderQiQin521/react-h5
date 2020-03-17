fca4f9bdd05d70e6750af7d2346ceb2d6de3be70
LionlyloveK <781517552@qq.com>
2020-03-17 11:13:54 +0800

    扫码确认完成

---
+++ src/pages/signin/affirm.tsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Button, WingBlank } from 'antd-mobile';
+
+import './affirm.less';
+class affirm extends Component {
+  qiandao(e) {
+    console.log('签到');
+    console.log(this);
+    console.log(e);
+  }
+  render() {
+    return (
+      <div className="affirm">
+        <div className="heard">
+          <h3 className="margin-bottom-xs">XXX会议（这里是会议名称）</h3>
+          <p>签到类型：大会现场签到</p>
+          <p>参会人员：张XXX</p>
+        </div>
+        <WingBlank>
+          <Button
+            type="primary"
+            className="btn"
+            onClick={this.qiandao.bind(this, 1)}
+          >
+            确认签到
+          </Button>
+        </WingBlank>
+      </div>
+    );
+  }
+}
+
+export default affirm;
