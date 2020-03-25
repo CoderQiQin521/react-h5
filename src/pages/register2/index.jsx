b9de99805496043cab2a6a7300356304a3977732
CoderQiQin521 <coderqiqin@aliyun.com>
2020-03-25 16:55:29 +0800

    更新

---
+++ src/pages/register2/index.jsx
@@ -0,0 +1,120 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+import styles from './style.js';
+
+class Page_0 extends Component {
+  render() {
+    return (
+      <div style={styles.box}>
+        <div style={styles.hd}>
+          <div style={styles.wrap}>
+            <img style={styles.subtract} src={'./images/img_25025_0_5.png'} />
+            <img
+              style={styles.FlipFlopsFill}
+              src={'./images/img_25025_0_1.png'}
+            />
+            <img
+              style={styles.FlipFlopsFill_2}
+              src={'./images/img_25025_0_1.png'}
+            />
+            <span style={styles.chinaUnicom}>中国联通</span>
+            <img
+              style={styles.leftbariconmenber}
+              src={'./images/img_25025_0_0.png'}
+            />
+            <span style={styles.wam}>114W.AM</span>
+          </div>
+          <div style={styles.block}>
+            <img style={styles.lanya} src={'./images/img_25025_0_6.png'} />
+            <span style={styles.percent}>77%</span>
+            <img style={styles.mark} src={'./images/img_25025_0_3.png'} />
+          </div>
+        </div>
+        <div style={styles.bd}>
+          <img style={styles.icon} src={'./images/img_25025_0_8.png'} />
+          <span style={styles.registration}>注册</span>
+        </div>
+        <div style={styles.submain} />
+        <span style={styles.row}>阿里巴巴全体会议</span>
+        <span style={styles.row1}>会议时间：2020年2月23日</span>
+        <div style={styles.row2} />
+        <span style={styles.row3}>填写报名信息</span>
+        <span style={styles.row4}>姓名</span>
+        <div style={styles.row5}>
+          <div style={styles.zhangYushengWrap}>
+            <span style={styles.zhangYusheng}>张雨生</span>
+          </div>
+        </div>
+        <span style={styles.row6}>手机号</span>
+        <div style={styles.row7}>
+          <div style={styles.container}>
+            <span style={styles.num}>1833569884</span>
+          </div>
+        </div>
+        <span style={styles.row8}>密码</span>
+        <div style={styles.row9}>
+          <div style={styles.outer}>
+            <span style={styles.word}>ab123578</span>
+          </div>
+        </div>
+        <span style={styles.title}>★数字+字母，长度6-16个字符</span>
+        <span style={styles.main}>确认密码</span>
+        <div style={styles.row10}>
+          <div style={styles.wrap_2}>
+            <span style={styles.text}>ab123578</span>
+          </div>
+        </div>
+        <span style={styles.row11}>电子邮箱</span>
+        <div style={styles.row12}>
+          <div style={styles.qcomWrap}>
+            <span style={styles.qcom}>23578828q.com</span>
+          </div>
+        </div>
+        <div style={styles.row13}>
+          <span style={styles.txt}>*</span>
+          <span style={styles.desc}>请务必填写有效的邮箱地址</span>
+        </div>
+        <span style={styles.row14}>性别</span>
+        <div style={styles.row15}>
+          <img
+            style={styles.backstageiconchose}
+            src={'./images/img_25025_0_7.png'}
+          />
+          <img style={styles.iconTest} src={'./images/img_25025_0_4.png'} />
+          <img
+            style={styles.CenRadioHIcon}
+            src={'./images/img_25025_0_9.png'}
+          />
+          <img style={styles.shanchu} src={'./images/img_25025_0_2.png'} />
+        </div>
+        <span style={styles.row16}>选择民族</span>
+        <div style={styles.row17}>
+          <div style={styles.group}>
+            <span style={styles.hanNationality}>汉族</span>
+            <img style={styles.down} src={'./images/img_25025_0_10.png'} />
+          </div>
+        </div>
+        <span style={styles.row18}>单位</span>
+        <div style={styles.row19}>
+          <div style={styles.color} />
+        </div>
+        <span style={styles.row20}>部门</span>
+        <div style={styles.row21}>
+          <div style={styles.colorDiv} />
+        </div>
+        <span style={styles.row22}>职务</span>
+        <div style={styles.row23}>
+          <div style={styles.color_2} />
+        </div>
+        <div style={styles.ft}>
+          <div style={styles.submitSaveWrap}>
+            <span style={styles.submitSave}>提交保存</span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Page_0;
